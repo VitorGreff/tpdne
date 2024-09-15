4149c9c5b434f3f2abbcf37c5a3dba41b62be2d4
VitorGreff <vitorgreffdias@gmail.com>
2024-09-15 15:17:18 -0300

    fix: error handling

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
-import { fetchImage } from "./utils/Image";
+import { fetchImage, saveImage } from "./utils/Image";
+import fs from 'fs/promises'
 
 async function main() {
-  try {
-    const result = await fetchImage('https://thispersondoesnotexist.com')
-    console.log(`Fetched image of type: ${result.contentType}`)
-    console.log(`Fetched image of size: ${result.blob.size}`)
-  } catch (e) {
-    if (e instanceof Error) {
-      process.stderr.write(`error: ${e.message}`)
-    } else {
-      process.stderr.write(`unknown error ocurred`)
+  while (true) {
+    try {
+      const result = await fetchImage('https://thispersondoesnotexist.com')
+      console.log(`TPDNE: Fetched image of type: ${result.contentType}`)
+      console.log(`TPDNE: Fetched image of size: ${result.blob.size}`)
+
+      const filePath = './images/test.jpeg'
+      await fs.mkdir('./images', { recursive: true })
+      await saveImage(result.blob, filePath)
+
+    } catch (e) {
+      if (e instanceof Error) {
+        process.exit(1)
+      } else {
+        process.stderr.write(`unknown error ocurred`)
+      }
     }
   }
 }
--- utils/Image.ts
+++ utils/Image.ts
@@ -1,4 +1,7 @@
+import fs from 'fs/promises'
+
 type FetchedImage = {
+  // Blob is a type that represents a binary data in the browser
   blob: Blob;
   contentType: string;
 }
@@ -15,16 +18,31 @@ export async function fetchImage(url: string): Promise<FetchedImage> {
     if (!contentType || !contentType.startsWith('image/')) {
       throw new Error(`Unexpected content type: ${contentType}`)
     }
-
     const blob = await response.blob()
     return { blob, contentType }
+
   } catch (e) {
     if (e instanceof Error) {
-      process.stderr.write(`Error fetching image" ${e.message}"`)
+      console.error(`Error fetching image" ${e.message}"`)
     } else {
-      process.stderr.write(`Unknow error ocurred while fetching image`)
+      console.error(`Unknow error ocurred while fetching image`)
     }
     throw e
   }
 }
 
+export async function saveImage(imageBlog: Blob, filePath: string): Promise<void> {
+  try {
+    const arrayBuffer = await imageBlog.arrayBuffer()
+    const uint8Array = new Uint8Array(arrayBuffer)
+    await fs.writeFile(filePath, uint8Array)
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(`Error saving image" ${e.message}"`)
+    } else {
+      console.error(`Unknow error ocurred while saving image`)
+    }
+    throw e
+  }
+
+}
